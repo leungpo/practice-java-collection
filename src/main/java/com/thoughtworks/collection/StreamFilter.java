09f1ffd30cc9d6f1ae4985fef4507267a0177f9f
leungpo <polly.leung@oocl.com>
2022-11-30 11:53:24 +0800

    feat: finish streamFilter

--- src/main/java/com/thoughtworks/collection/StreamFilter.java
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -1,20 +1,32 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamFilter {
 
     public List<Integer> filterEven(List<Integer> numbers) {
-        return null;
+        List<Integer> evenNumber = numbers.stream()
+                                            .filter(number -> number % 2 == 0)
+                                            .collect(Collectors.toList());
+
+        return evenNumber;
     }
 
     public List<String> filterEndWithS(List<String> words) {
-        return null;
+        List<String> wordEndWithS = words.stream()
+                                                .filter(word -> word.endsWith("s"))
+                                                .collect(Collectors.toList());
+
+        return wordEndWithS;
     }
 
     public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
-        return null;
+        List<Integer> commonElements = numbers.stream()
+                .filter(number -> anotherNumbers.contains(number))
+                .collect(Collectors.toList());
+        return commonElements;
     }
 
 
